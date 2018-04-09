4b669a043d1fd21d2f68bd38a8d226d910cc8655
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-04-09 10:29:24 -0700

    Helpers

---
+++ TestHelpers/Helpers/ContextHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestHelpers.Helpers
+{
+    public class ContextHelper<TContext> where TContext : Microsoft.EntityFrameworkCore.DbContext , IDisposable
+    {
+        private SqliteConnection Connection { get; }
+        public TContext Context { get;}
+
+        public ContextHelper()
+        {
+            Connection = new SqliteConnection("DataSource=:memory:");
+            Connection.Open();
+
+            var options = new DbContextOptionsBuilder<TContext>()
+                .UseSqlite(Connection)
+                .Options;
+            Context = (TContext)Activator.CreateInstance(typeof(TContext), options); //Not sure if this is correct...
+            //Context = new TContext(options);
+            Context.Database.EnsureCreated();
+        }
+
+
+        public void Dispose()
+        {
+            Connection?.Close();
+        }
+    }
+}
